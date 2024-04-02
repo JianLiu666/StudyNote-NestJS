266c2f1f226cd10946eef1354395c035c6070f9c
jian.liu <jianliu0616@gmail.com>
2024-04-02 23:07:06 +0800

    feat: update controller

--- apps/ithelp-d02-controller/src/todo/todo.controller.ts
+++ apps/ithelp-d02-controller/src/todo/todo.controller.ts
@@ -1,10 +1,12 @@
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
   Param,
   Patch,
+  Post,
   Query,
 } from '@nestjs/common';
 
@@ -77,8 +79,30 @@ export class TodoController {
   }
 
   @Patch()
-  @HttpCode(HttpStatus.NO_CONTENT)
+  @HttpCode(HttpStatus.NO_CONTENT) // 使用 decorator 回傳指定狀態碼
   patch() {
     return [];
+  }
+
+  // 主體資料寫法1
+  @Post('create1')
+  create1(@Body() data: { title: string; description?: string }) {
+    const id = 1;
+    return {
+      id,
+      // ...data 會把 data 物件的所有屬性都複製到新物件, 如果 data 物件也有 id 屬性, 這樣寫會覆蓋掉 id
+      // 通常被用在要更新物件的部分屬性時, 例如想更新 id 又要保留其他屬性
+      ...data,
+    };
+  }
+
+  // 主體資料寫法2
+  @Post('create2')
+  create2(
+    @Body('title') title: string,
+    @Body('description') description?: string,
+  ) {
+    const id = 1;
+    return { id, title, description };
   }
 }
